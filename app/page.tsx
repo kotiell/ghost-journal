22b13a378b36ee619fd9abf0cbd669da53b0ea25
kotiell <tielljustin@gmail.com>
2023-03-12 17:17:29 -0400

    removed await function in journal

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Journal() {
 
-  const ghosts = await fetch(`${process.env.BASE_PATH}/api/database-connect`)
-    .then((response) => response.json())
+  // const ghosts = await fetch(`${process.env.BASE_PATH}/api/database-connect`)
+  //   .then((response) => response.json())
   //console.log('ghosts', ghosts);
   return (
     <div>
