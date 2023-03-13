b6320c85e2a9e6fccb3a2888e9c37a0ff81675bb
kotiell <tielljustin@gmail.com>
2023-03-12 20:46:23 -0400

    color update

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -9,13 +9,13 @@ export default function Toggle() {
     <Switch
       checked={enabled}
       onChange={setEnabled}
-      className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'
+      className={`${enabled ? 'bg-orange-600' : 'bg-orange-200'
         } relative inline-flex h-6 w-11 items-center rounded-full`}
     >
       <span className="sr-only">Enable notifications</span>
       <span
         className={`${enabled ? 'translate-x-6' : 'translate-x-1'
-          } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+          } inline-block h-4 w-4 transform rounded-full bg-black transition`}
       />
     </Switch>
   )
