ef1f030af2ad9ab377cd6dd65f6d91d08c13722b
kotiell <tielljustin@gmail.com>
2023-03-18 11:38:31 -0400

    updated scema for ghost evidence

--- components/JournalContent.tsx
+++ components/JournalContent.tsx
@@ -69,6 +69,50 @@ export default function JournalContent({ ghosts, evidence }: IJournalContentProp
   return (
     <div>
       <h1 className="text-5xl font-bold mb-8 mt-12">GhostGetter Journal</h1>
+      {evidence && (
+        <div className="grid grid-cols-3 gap-4 mb-12">
+          {evidence.map((evidence, index) => {
+            const evidenceFound = foundEvidence.includes(evidence.name);
+            const evidenceClassName = evidenceFound ? "text-green-500 font-bold" : "";
+            const ghostsWithEvidence = ghosts.filter((ghost) => {
+              return ghost.evidence.find((e) => e.name === evidence.name);
+            });
+            const ghostNamesWithEvidence = ghostsWithEvidence.map((ghost) => ghost.name);
+
+            return (
+              <div key={index} className="flex flex-col justify-between">
+                <div className={`text-xl font-bold ml-2 ${evidenceClassName}`}>{evidence.name} </div>
+                <div className="ml-2">
+                  {ghostsWithEvidence.length > 0 && (
+                    <span>
+                      Found with:{" "}
+                      <br />
+                      {ghostNamesWithEvidence.map((name, i) => (
+                        <span key={i} className="text-green-500 font-bold">
+                          {name}
+                          {i < ghostNamesWithEvidence.length - 1 ? ", " : ""}
+                        </span>
+                      ))}
+                    </span>
+                  )}
+                </div>
+                <button
+                  onClick={() =>
+                    setFoundEvidence(
+                      evidenceFound
+                        ? foundEvidence.filter((e) => e !== evidence.name)
+                        : [...foundEvidence, evidence.name]
+                    )
+                  }
+                  className={`h-12 w-full rounded px-4 py-2 ${evidenceFound ? "bg-green-700 text-white" : "bg-yellow-300 text-black "}`}
+                >
+                  {evidenceFound ? "Found" : "Not found"}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      )}
       {ghosts && <h2 className={sectionTitleClassName}>Ghosts</h2>}
       {ghosts && (
         <div className="grid grid-cols-3 gap-4 mb-12">
@@ -94,51 +138,7 @@ export default function JournalContent({ ghosts, evidence }: IJournalContentProp
           })}
         </div>
       )}
-      {evidence && (
-        <div className="grid grid-cols-3 gap-4 mb-12">
-          {evidence.map((evidence, index) => {
-            const evidenceFound = foundEvidence.includes(evidence.name);
-            const evidenceClassName = evidenceFound ? "text-green-500 font-bold" : "";
-            const ghostsWithEvidence = ghosts.filter((ghost) => {
-              return ghost.evidence.find((e) => e.name === evidence.name);
-            });
-            const ghostNamesWithEvidence = ghostsWithEvidence.map((ghost) => ghost.name);
 
-            return (
-              <div key={index}>
-                <div className="flex">
-                  <button
-                    onClick={() =>
-                      setFoundEvidence(
-                        evidenceFound
-                          ? foundEvidence.filter((e) => e !== evidence.name)
-                          : [...foundEvidence, evidence.name]
-                      )
-                    }
-                    className={evidenceFound ? "bg-green-500" : ""}
-                  >
-                    {evidenceFound ? "Found" : "Not found"}
-                  </button>
-                  <div className={`ml-2 ${evidenceClassName}`}>{evidence.name} </div>
-                </div>
-                <div className="ml-4">
-                  {ghostsWithEvidence.length > 0 && (
-                    <span>
-                      Found with:{" "}
-                      {ghostNamesWithEvidence.map((name, i) => (
-                        <span key={i} className="text-green-500 font-bold">
-                          {name}
-                          {i < ghostNamesWithEvidence.length - 1 ? ", " : ""}
-                        </span>
-                      ))}
-                    </span>
-                  )}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
     </div>
   )
 }
